b10bece9c70d9b4a9c00b88e8d94ba86b22e2b2e
Bret Doucette <doucette.bret@gmail.com>
2015-12-09 11:23:59 -0700

    Updated ball tests

--- test/balls-test.js
+++ test/balls-test.js
@@ -11,22 +11,22 @@ describe('balls', function () {
   });
 
   it('should have a X-coordinate', function () {
-    let ball = new Ball(3, 4, 20, 29);
+    let ball = new Ball();
     assert.equal(ball.x, 320);
   });
 
   it('should have a Y-coordinate', function(){
-    let ball = new Ball(3, 4, 20, 29);
+    let ball = new Ball();
     assert.equal(ball.y, 360);
   });
 
   it('should have a radius', function(){
-    let ball = new Ball(3, 4, 20, 29);
-    assert.equal(ball.radius, 20);
+    let ball = new Ball();
+    assert.equal(ball.radius, 10);
   });
 
   it('should have a startAngle', function(){
-    let ball = new Ball(3, 4, 20, 29);
-    assert.equal(ball.startAngle, 29);
+    let ball = new Ball();
+    assert.equal(ball.startAngle, 0);
   });
 });
