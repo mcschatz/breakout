7415fa508c4d6e7b5229f6bddc8db06b26daeec4
Bret Doucette <doucette.bret@gmail.com>
2015-12-13 09:55:22 -0700

    Added test files - still need to write them

---
+++ test/start-test.js
@@ -0,0 +1,12 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+const Start = require('../lib/start');
+
+describe('start', function () {
+
+  it('should instatiate a new start', function () {
+    let start = new Start();
+    assert.isObject(ball);
+  });
+});
