ec05fac1dd1e740738f5741a5ed547e08582d74e
Mimi Schatz <mcschatz@gmail.com>
2015-12-14 14:55:54 -0700

    npm run build

--- main.bundle.js
+++ main.bundle.js
@@ -42,15 +42,403 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var Start = __webpack_require__(1);
+
+	new Start();
+
+/***/ },
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var Game = __webpack_require__(2);
+
+	var Start = function Start() {
+
+	  if (document.getElementById('canvas')) {
+	    var canvasId = document.getElementById("canvas");
+	    var canvas = document.getElementById('canvas').getContext('2d');
+	  } else {
+	    var canvasId = document.createElement('canvas');
+	    canvasId.width = 782;
+	    canvasId.height = 520;
+	    var canvas = canvasId.getContext('2d');
+	  }
+
+	  this.size = { x: canvas.canvas.width, y: canvas.canvas.height };
+	  this.themeSound = document.getElementById("theme-sound");
+	  this.themeSound.load();
+	  this.themeSound.play();
+
+	  var self = this;
+	  var start = function start(canvas, canvasId) {
+	    self.draw(canvas);
+	    self.begin(canvasId, self);
+	  };
+	  start(canvas, canvasId);
+	};
+
+	Start.prototype = {
+
+	  draw: function draw(canvas) {
+	    var y = this.size.y / 3;
+
+	    function centerText(canvas, text, y) {
+	      var measurement = canvas.measureText(text);
+	      var x = (canvas.canvas.width - measurement.width) / 2;
+	      canvas.fillText(text, x, y);
+	    }
+
+	    canvas.clearRect(0, 0, this.size.x, this.size.y);
+	    canvas.fillStyle = 'white';
+	    canvas.font = '48px monospace';
+	    centerText(canvas, 'Break Out', y);
+
+	    canvas.fillStyle = "white";
+	    canvas.font = '24px monospace';
+	    centerText(canvas, 'Click Anywhere to Begin!', y + 40);
+	  },
+
+	  begin: function begin(canvasId, self) {
+
+	    canvasId.addEventListener('click', function game() {
+	      new Game();
+	      function theme(self) {
+	        self.themeSound.volume = 1.00;
+	        for (var i = 0; i < 10; i++) {
+	          setTimeout((function (x) {
+	            return function () {
+	              self.themeSound.volume = self.themeSound.volume - 0.082;
+	            };
+	          })(i), 1000 * i);
+	        };
+	      }
+
+	      theme(self);
+	      canvasId.removeEventListener('click', game);
+	    });
+	  }
+	};
+
+	module.exports = Start;
+
+/***/ },
+/* 2 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var Ball = __webpack_require__(3);
+	var Paddle = __webpack_require__(5);
+	var Brick = __webpack_require__(6);
+	var Colors = __webpack_require__(7);
+	var Canvas = __webpack_require__(4);
+
+	var Game = function Game() {
+	  var canvas = new Canvas();
+
+	  this.size = { x: canvas.canvas.width, y: canvas.canvas.height };
+	  var paddle = new Paddle(this);
+	  var bricks = createBricks(this);
+
+	  this.bodies = bricks.concat(createBall(this, paddle, bricks)).concat(paddle);
+	  this.score = 0;
+	  this.lives = 3;
+	  this.paddleSound = document.getElementById("paddle-sound");
+	  this.topSound = document.getElementById("top-sound");
+	  this.wallSound = document.getElementById("wall-sound");
+	  this.deathSound = document.getElementById("death-sound");
+	  this.brickSound = document.getElementById("brick-sound");
+
+	  var self = this;
+	  var tick = function tick() {
+	    self.move();
+	    self.draw(canvas);
+	    self.drawScore(canvas);
+	    self.drawLives(canvas);
+	    requestAnimationFrame(tick);
+	  };
+	  tick();
+	};
+
+	var createBall = function createBall(game, paddle, bricks) {
+	  var ball = [];
+	  ball.push(new Ball(game, paddle, bricks));
+	  return ball;
+	};
+
+	var createBricks = function createBricks(game) {
+	  var bricks = [];
+	  for (var i = 0; i < 54; i++) {
+	    var x = Math.floor(i / 6) * 87;
+	    var y = 70 + i % 6 * 18;
+	    bricks.push(new Brick(game, { x: x, y: y }));
+	  };
+	  return bricks;
+	};
+
+	Game.prototype = {
+
+	  move: function move() {
+	    for (var i = 0; i < this.bodies.length; i++) {
+	      if (this.bodies[i].move !== undefined) {
+	        this.bodies[i].move();
+	      }
+	    }
+	  },
+
+	  draw: function draw(canvas) {
+	    canvas.clearRect(0, 0, this.size.x, this.size.y);
+	    var colorList = new Colors();
+	    for (var i = 0; i < this.bodies.length; i++) {
+	      if (this.bodies[i].draw !== undefined) {
+	        this.bodies[i].draw(canvas, colorList[i].toString());
+	      }
+	    }
+	  },
+
+	  drawScore: function drawScore(canvas) {
+	    canvas.font = '16px monospace';
+	    canvas.fillStyle = "#0095DD ";
+	    canvas.fillText("Score: " + this.score, 8, 20);
+	  },
+
+	  drawLives: function drawLives(canvas) {
+	    canvas.font = '16px monospace';
+	    canvas.fillStyle = "#0095DD ";
+	    canvas.fillText("Lives: " + this.lives, 700, 20);
+	  }
+	};
+
+	module.exports = Game;
+
+/***/ },
+/* 3 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+
+	var Canvas = __webpack_require__(4);
+
+	var dx = 5;
+	var dy = -5;
+
+	var Ball = function Ball(game, paddle, bricks) {
+	  var canvas = new Canvas().canvas;
+	  this.game = game;
+	  this.paddle = paddle;
+	  this.startAngle = 0;
+	  this.x = canvas.width / 2;
+	  this.y = canvas.height - 30;
+	  this.canvasHeightOffset = 8;
+	  this.radius = 10;
+	  this.gameSize = { x: this.game.size.x, y: this.game.size.y };
+	  this.bricks = bricks;
+	};
+
+	Ball.prototype = {
+
+	  draw: function draw(canvas) {
+	    canvas.beginPath();
+	    canvas.arc(this.x, this.y, this.radius, this.startAngle, Math.PI * 2);
+	    canvas.fillStyle = "#F00000";
+	    canvas.fill();
+	    canvas.closePath();
+	  },
+
+	  reset: function reset() {
+	    this.startAngle = 0;
+	    this.x = canvas.width / 2;
+	    this.y = canvas.height - 30;
+	    dy = -dy;
+	  },
+
+	  move: function move() {
+	    this.collisionDetectionBricks();
+
+	    if (this.x + dx > this.gameSize.x - this.radius || this.x + dx < this.radius) {
+	      this.game.wallSound.load();
+	      this.game.wallSound.play();
+	      dx = -dx;
+	    }
+
+	    if (this.y - this.canvasHeightOffset + dy < this.radius) {
+	      this.game.topSound.load();
+	      this.game.topSound.play();
+	      dy = -dy;
+	    } else if (this.y + dy > this.gameSize.y - this.radius * 2) {
+	      if (this.x > this.paddle.position.x && this.x < this.paddle.position.x + this.paddle.size.x) {
+	        this.game.paddleSound.load();
+	        this.game.paddleSound.play();
+	        dy = -dy;
+	      } else {
+
+	        this.game.lives -= 1;
+	        if (this.game.lives > 0) {
+	          this.reset();
+	        } else {
+	          alert("You Fell into the Wall - GAME OVER");
+	          this.game.deathSound.load();
+	          this.game.deathSound.play();
+	          document.location.reload();
+	        }
+	      }
+	    }
+	    this.x += dx;
+	    this.y += dy;
+	  },
+
+	  collisionDetectionBricks: function collisionDetectionBricks() {
+	    var self = this;
+	    var brickHeight = self.bricks[0].size.y;
+	    var brickWidth = self.bricks[0].size.x;
+
+	    for (var i = 0; i < self.bricks.length; i++) {
+	      if (self.x > self.bricks[i].position.x && self.x < self.bricks[i].position.x + brickWidth && self.y - self.radius < self.bricks[i].position.y + brickHeight && self.y + self.radius > self.bricks[i].position.y - brickHeight) {
+	        this.game.brickSound.load();
+	        this.game.brickSound.play();
+	        dy = -dy;
+	        self.bricks[i].status = 0;
+	        self.bricks.splice(i, 1);
+	        self.game.score += 10;
+	      }
+	    }
+	    return dy;
+	  }
+	};
+
+	module.exports = Ball;
+
+/***/ },
+/* 4 */
 /***/ function(module, exports) {
 
 	'use strict';
 
-	var sayHello = function sayHello() {
-	  return console.log('Hello');
+	var Canvas = function Canvas() {
+	    if (document.getElementById('canvas')) {
+	        var canvas = document.getElementById('canvas').getContext('2d');
+	        return canvas;
+	    } else {
+	        var context = document.createElement('canvas');
+	        context.width = 782;
+	        context.height = 520;
+	        var canvas = context.getContext('2d');
+	        return canvas;
+	    }
 	};
 
-	sayHello();
+	module.exports = Canvas;
+
+/***/ },
+/* 5 */
+/***/ function(module, exports) {
+
+	'use strict';
+
+	var Paddle = function Paddle(game) {
+	  this.game = game;
+	  this.size = { x: 100, y: 15 };
+	  this.gameSize = { x: this.game.size.x, y: this.game.size.y };
+	  this.position = { x: this.game.size.x / 2 - 50, y: this.game.size.y - 15 };
+	  this.keyboarder = new Keyboarder();
+	};
+
+	Paddle.prototype = {
+
+	  draw: function draw(canvas) {
+	    canvas.beginPath();
+	    canvas.rect(this.position.x, this.position.y, this.size.x, this.size.y);
+	    canvas.fillStyle = "#D03ACD";
+	    canvas.fill();
+	    canvas.closePath();
+	  },
+
+	  move: function move() {
+	    var paddle_move_speed = 12;
+
+	    if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT) && this.position.x > 0) {
+	      this.position.x -= paddle_move_speed;
+	    } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT) && this.position.x < this.gameSize.x - this.size.x) {
+	      this.position.x += paddle_move_speed;
+	    }
+	  }
+	};
+
+	var Keyboarder = function Keyboarder() {
+	  var keyState = {};
+	  document.addEventListener('keydown', function (e) {
+	    keyState[e.keyCode] = true;
+	  });
+
+	  document.addEventListener('keyup', function (e) {
+	    keyState[e.keyCode] = false;
+	  });
+
+	  this.isDown = function (keyCode) {
+	    return keyState[keyCode] === true;
+	  };
+
+	  this.KEYS = { LEFT: 37, RIGHT: 39 };
+	};
+
+	module.exports = Paddle;
+
+/***/ },
+/* 6 */
+/***/ function(module, exports) {
+
+	"use strict";
+
+	var Brick = function Brick(game, position) {
+	  this.game = game;
+	  this.position = position;
+	  this.size = { x: 87, y: 18 };
+	  this.status = 1;
+	};
+
+	Brick.prototype = {
+
+	  draw: function draw(canvas, color) {
+	    if (this.status === 1) {
+	      canvas.beginPath();
+	      canvas.rect(this.position.x, this.position.y, this.size.x, this.size.y);
+	      canvas.fillStyle = color;
+	      canvas.fill();
+	      canvas.closePath();
+	    }
+	  }
+	};
+
+	module.exports = Brick;
+
+/***/ },
+/* 7 */
+/***/ function(module, exports) {
+
+	"use strict";
+
+	var Colors = function Colors() {
+	  var colorWheel = [];
+	  for (var i = 0; i < 10; i++) {
+	    if (i < 10) {
+	      colorWheel.push("#CC3ED0");
+	      colorWheel.push("#F95255");
+	      colorWheel.push("#FE801A");
+	      colorWheel.push("#FF911B");
+	      colorWheel.push("#10AC24");
+	      colorWheel.push("#6667FF");
+	    }
+	  };
+	  return colorWheel;
+	};
+
+	module.exports = Colors;
 
 /***/ }
 /******/ ]);
