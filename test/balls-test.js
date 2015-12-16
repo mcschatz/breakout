e7bb8d058f2d8204de81709b0a2f6a94c06d3f2a
Bret Doucette <doucette.bret@gmail.com>
2015-12-15 17:16:59 -0700

    Removed comments from code

--- test/balls-test.js
+++ test/balls-test.js
@@ -21,25 +21,21 @@ describe('balls', function () {
 
   it('should have a X-coordinate', function () {
     let ball = new Ball(this.game, this.paddle, this.brick);
-    // let ball = new Ball(this.game);
     assert.equal(ball.x, 391);
   });
 
   it('should have a Y-coordinate', function(){
     let ball = new Ball(this.game, this.paddle, this.brick);
-    // let ball = new Ball(this.game);
     assert.equal(ball.y, 495);
   });
 
   it('should have a radius', function(){
     let ball = new Ball(this.game, this.paddle, this.brick);
-    // let ball = new Ball(this.game);
     assert.equal(ball.radius, 10);
   });
 
   it('should have a startAngle', function(){
     let ball = new Ball(this.game, this.paddle, this.brick);
-    // let ball = new Ball(this.game);
     assert.equal(ball.startAngle, 0);
   });
 });
