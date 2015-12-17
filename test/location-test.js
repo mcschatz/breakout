948899dde92b15b6806ea988b138c0b73ca752f7
Mimi Schatz <mcschatz@gmail.com>
2015-12-17 12:17:24 -0700

    added tests for wall collision functions in location

--- test/location-test.js
+++ test/location-test.js
@@ -17,38 +17,53 @@ describe('location', function () {
   });
 
   it('should recognize the outterLeft of the paddle', function () {
-    this.ball.x = 50
-    this.paddle.position.x = 40
+    this.ball.x = 50;
+    this.paddle.position.x = 40;
     assert.equal(Location.outterLeft(this.ball), true);
   });
 
   it('should recognize the innerLeft of the paddle', function () {
-    this.ball.x = 100
-    this.paddle.position.x = 75
+    this.ball.x = 100;
+    this.paddle.position.x = 75;
     assert.equal(Location.innerLeft(this.ball), true);
   });
 
   it('should recognize the centerLeft of the paddle', function () {
-    this.ball.x = 150
-    this.paddle.position.x = 90
+    this.ball.x = 150;
+    this.paddle.position.x = 90;
     assert.equal(Location.centerLeft(this.ball), true);
   });
 
   it('should recognize the centerRight of the paddle', function () {
-    this.ball.x = 130
-    this.paddle.position.x = 40
+    this.ball.x = 130;
+    this.paddle.position.x = 40;
     assert.equal(Location.centerRight(this.ball), true);
   });
 
   it('should recognize the innerRight of the paddle', function () {
-    this.ball.x = 150
-    this.paddle.position.x = 40
+    this.ball.x = 150;
+    this.paddle.position.x = 40;
     assert.equal(Location.innerRight(this.ball), true);
   });
 
   it('should recognize the outterRight of the paddle', function () {
-    this.ball.x = 180
-    this.paddle.position.x = 40
+    this.ball.x = 180;
+    this.paddle.position.x = 40;
     assert.equal(Location.outterRight(this.ball), true);
+  });
+
+  it('should recognize the sideWalls', function () {
+    this.ball.x = 0;
+    assert.equal(Location.sideWalls(this.ball), true);
+  });
+
+  it('should recognize the topWall', function () {
+    this.ball.y = 0;
+    assert.equal(Location.topWall(this.ball), true);
+  });
+
+  it('should recognize the bottomWall', function () {
+    this.ball.y = 530;
+    assert.equal(Location.bottomWall(this.ball), true);
   });
 });
