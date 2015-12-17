c2c894e0923401129fce8a58709589d5fe18918c
Mimi Schatz <mcschatz@gmail.com>
2015-12-16 22:50:47 -0700

    added tests for wall collision

--- test/balls-test.js
+++ test/balls-test.js
@@ -12,72 +12,89 @@ describe('balls', function () {
     this.game = new Game();
     this.paddle = new Paddle(this.game);
     this.brick = new Brick(this.game);
+    this.ball = new Ball(this.game, this.paddle, this.brick);
   });
 
   it('should instatiate a new ball', function () {
-    let ball = new Ball(this.game, this.paddle, this.brick);
-    assert.isObject(ball);
+    assert.isObject(this.ball);
   });
 
   it('should have a X-coordinate', function () {
-    let ball = new Ball(this.game, this.paddle, this.brick);
-    assert.equal(ball.x, 391);
+    assert.equal(this.ball.x, 391);
   });
 
   it('should have a Y-coordinate', function(){
-    let ball = new Ball(this.game, this.paddle, this.brick);
-    assert.equal(ball.y, 495);
+    assert.equal(this.ball.y, 495);
   });
 
   it('should have a radius', function(){
-    let ball = new Ball(this.game, this.paddle, this.brick);
-    assert.equal(ball.radius, 10);
+    assert.equal(this.ball.radius, 10);
   });
 
   it('should have a startAngle', function(){
-    let ball = new Ball(this.game, this.paddle, this.brick);
-    assert.equal(ball.startAngle, 0);
+    assert.equal(this.ball.startAngle, 0);
   });
 
   it('should have a velocity', function(){
-    let ball = new Ball(this.game, this.paddle, this.brick);
-    assert.equal(ball.dy, 10);
+    assert.equal(this.ball.dy, 10);
   });
 
   it('should have a gameSize', function(){
-    let ball = new Ball(this.game, this.paddle, this.brick);
-    assert.equal(ball.gameSize.x, 782);
-    assert.equal(ball.gameSize.y, 520);
+    assert.equal(this.ball.gameSize.x, 782);
+    assert.equal(this.ball.gameSize.y, 520);
   });
 
   it('should update a ball direction based on where it hits the paddle', function(){
-    let ball = new Ball(this.game, this.paddle, this.brick);
-    assert.equal(ball.dx, -10);
-    assert.equal(ball.dy, 10);
+    assert.equal(this.ball.dx, -10);
+    assert.equal(this.ball.dy, 10);
 
-    ball.setLeftSlope();
-    assert.equal(ball.dx, -13);
-    assert.equal(ball.dy, -8);
+    this.ball.setLeftSlope();
+    assert.equal(this.ball.dx, -13);
+    assert.equal(this.ball.dy, -8);
 
-    ball.setLeftCenterSlope();
-    assert.equal(ball.dx, -11);
-    assert.equal(ball.dy, -9);
+    this.ball.setLeftCenterSlope();
+    assert.equal(this.ball.dx, -11);
+    assert.equal(this.ball.dy, -9);
 
-    ball.setLeftCenterCenterSlope();
-    assert.equal(ball.dx, -10);
-    assert.equal(ball.dy, -10);
+    this.ball.setLeftCenterCenterSlope();
+    assert.equal(this.ball.dx, -10);
+    assert.equal(this.ball.dy, -10);
 
-    ball.setRightCenterCenterSlope();
-    assert.equal(ball.dx, 10);
-    assert.equal(ball.dy, -10);
+    this.ball.setRightCenterCenterSlope();
+    assert.equal(this.ball.dx, 10);
+    assert.equal(this.ball.dy, -10);
 
-    ball.setRightCenterSlope();
-    assert.equal(ball.dx, 11);
-    assert.equal(ball.dy, -9);
+    this.ball.setRightCenterSlope();
+    assert.equal(this.ball.dx, 11);
+    assert.equal(this.ball.dy, -9);
 
-    ball.setRightSlope();
-    assert.equal(ball.dx, 13);
-    assert.equal(ball.dy, -8);
+    this.ball.setRightSlope();
+    assert.equal(this.ball.dx, 13);
+    assert.equal(this.ball.dy, -8);
+  });
+
+  it('should detect a right wall collision', function(){
+    this.ball.x = 800
+    assert.equal(this.ball.dx, -10);
+
+    this.ball.collisionDetectionWalls();
+    assert.equal(this.ball.dx, 10);
+  });
+
+  it('should detect a left wall collision', function(){
+    this.ball.x = 0
+    assert.equal(this.ball.dx, -10);
+
+    this.ball.collisionDetectionWalls();
+    assert.equal(this.ball.dx, 10);
+  });
+
+  it('should detect a top wall collision', function(){
+    this.ball.y = 0
+    assert.equal(this.ball.dy, 10);
+
+    this.ball.collisionDetectionWalls();
+    assert.equal(this.ball.dx, -10);
   });
 });
 
